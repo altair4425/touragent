2a9c309683476810487b19e2205479c35410500f
altair4425 <altair4425@gmail.com>
2012-01-26 13:08:52 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Touragent1::Application.config.secret_token = '99dfd290704ceaac521e22d775ef7de74ca30729ba871f9d31377bf6d2cc4e52df982b2098401c8a53272ff2d492f8e528d1096d2a4936ab7bcc8fd768d740fd'
